bc97a520ad611fd7b6ca48c49807518293e2c5fb
Brian Sander <hi@briansander.com>
2023-03-24 02:52:33 -0400

    update style, increase intro text, add emoji anim to logo

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ const navSections = document.querySelectorAll('section')
 const closeButtons = document.querySelectorAll('button.shadeContent__close')
 const shade = document.querySelector('div.shade')
 const nav = document.querySelector('nav')
+const emojis = [
+  "😀", "😁", "😂", "🤣", "😃", "😄", "😅", "😆", "😉", "😊", "😋", "😎", "😍", "😘", "😗", "😙", "😚", "😛", "🤔", "🤨", 
+  "😐", "😑", "😶", "🙄", "😏", "😣", "😥", "😮", "🤐", "😯", "😪", "😫", "😴", "😌", "😛", "😜", "😝", "🤤", "😒", "😓", 
+  "😔", "😕", "🙃", "🤑", "😲", "☹️", "🙁", "😖", "😞", "😟", "😤", "😢", "😭", "😦", "😧", "😨", "😩", "🤯", "😬", "😰", 
+  "😱", "😳", "🤪", "😵", "😡", "😠", "🤬", "😷", "🤒", "🤕", "🤮", "🤧", "😇", "🤠", "🥳", "😈", "👿", "👹", 
+  "👺", "💀", "👻", "👽", "🤖", "😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿", "😾"
+];
+
 
 let clickedButton = ""
 
@@ -45,3 +53,57 @@ function closeShade(){
   nav.classList.remove('-inverse')
   shade.classList.remove('-reveal')
 }
+
+const selector = document.querySelector('.swapper');
+const titles = ["Hero of time", "dragon slayer", "mentor", "creator", "prompt writer", "dreamer", "vocaloid", "space cowboy", "game addict"];
+
+let currentTitle = "";
+let index = 0;
+let interval;
+
+const changeText = () => {
+  if (index >= currentTitle.length) {
+    clearInterval(interval);
+    setTimeout(() => {
+      const newTitle = titles[Math.floor(Math.random() * titles.length)];
+      if (newTitle !== currentTitle) {
+        currentTitle = newTitle;
+        index = 0;
+      }
+      interval = setInterval(changeText, 80);
+    }, 2000);
+    return;
+  }
+  selector.innerHTML = currentTitle.substring(0, index + 1);
+  index++;
+};
+
+interval = setInterval(changeText, 100);
+
+const logo = document.querySelector('.-logo');
+
+logo.addEventListener('mousedown', () => {
+  const emoji = document.createElement('div');
+  emoji.classList.add('emoji');
+  emoji.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+
+  const { left, top, width, height } = logo.getBoundingClientRect();
+  emoji.style.left = `${left - (width / 5)}px`;
+  emoji.style.top = `${top - (height / 2)}px`;
+
+  document.body.appendChild(emoji);
+
+  setTimeout(() => {
+    emoji.style.transform = `scale(1.5)`;
+    emoji.style.transition = `all 0.5s cubic-bezier(0.42, 0, 0.58, 1)`;
+  }, 80);
+
+  emoji.addEventListener('mouseleave', () => {
+    emoji.style.transform = `scale(0)`;
+    emoji.style.transition = `all 0.5s cubic-bezier(0.42, 0, 0.58, 1)`;
+
+    setTimeout(() => {
+      emoji.remove();
+    }, 500);
+  });
+});
