992d16049cf0346f1fa432db5c1dff45ea526dd9
bhomie <hi@briansander.com>
2023-04-27 11:13:17 -0400

    Added logo click to close shade, removed close btn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const navButtons = document.querySelectorAll('button.nav__button')
 const navSections = document.querySelectorAll('section')
-const closeButtons = document.querySelectorAll('button.shadeContent__close')
 const shade = document.querySelector('div.shade')
 const nav = document.querySelector('nav')
 const swapper = document.querySelector('.swapper');
@@ -14,83 +13,78 @@ const emojis = [
   "👺", "💀", "👻", "👽", "🤖", "😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿", "😾"
 ];
 
-let index = 0;
-
 // Event listeners
 navButtons.forEach(button => button.addEventListener('click', revealSection));
-closeButtons.forEach(button => button.addEventListener('click', closeShade));
 
-// functions 
-
+// Functions
 function revealSection(e) {
+  nav.classList.add('-inverse');
+  shade.classList.add('-reveal');
+
   navButtons.forEach((button, index) => {
     const section = navSections[index];
     const isActive = e.target.getAttribute('navTo') === section.getAttribute('content');
     
     button.classList.toggle('-active', isActive);
     section.classList.toggle('-reveal', isActive);
-    
-    // const paragraphs = section.querySelectorAll('.shadeContent__body');
-    const paragraphs = section.querySelectorAll('h2,p');
-    paragraphs.forEach((paragraph, i) => {
-      paragraph.classList.toggle('-fadeIn', isActive);
-      paragraph.style.animationDelay = isActive ? `${i * 300}ms` : '';
-    });
-  });
+    animateParagraphs(section, isActive);
 
-  nav.classList.add('-inverse');
-  shade.classList.add('-reveal');
+    if (isActive && button.classList.contains('-active')) {
+      button.removeEventListener('click', revealSection);
+      button.addEventListener('click', closeShade);
+    } else {
+      button.removeEventListener('click', closeShade);
+      button.addEventListener('click', revealSection);
+    }
+  });
+}
+
+function animateParagraphs(section, isActive) {
+  const paragraphs = section.querySelectorAll('h2,p');
+  paragraphs.forEach((paragraph, i) => {
+    paragraph.classList.toggle('-fadeIn', isActive);
+    paragraph.style.animationDelay = isActive ? `${i * 300}ms` : '';
+  });
 }
 
 function closeShade() {
   navButtons.forEach((button, index) => {
-    button.classList.remove('-active')
-    navSections[index].classList.remove('-reveal')
-  })
+    button.classList.remove('-active');
+    navSections[index].classList.remove('-reveal');
+  });
+  //fix this in future - bug: clicking logo out of a shade will make the button unclickable
+  navButtons.forEach(button => button.addEventListener('click', revealSection));
 
-  nav.classList.remove('-inverse')
-  shade.classList.remove('-reveal')
+  nav.classList.remove('-inverse');
+  shade.classList.remove('-reveal');
 }
 
-const titleChange = () => {
-  let currentTitle = swapper.textContent.trim();
-  let availableTitles = titles.filter(title => title !== currentTitle);
-  let randomIndex = Math.floor(Math.random() * availableTitles.length);
-  let randomTitle = availableTitles[randomIndex] + '.';
+// Title Change
+const titleChange = async () => {
+  const currentTitle = swapper.textContent.trim();
+  const availableTitles = titles.filter(title => title !== currentTitle);
+  const randomTitle = availableTitles[Math.floor(Math.random() * availableTitles.length)] + '.';
   const msWait = 150;
-  let spaceCounter = 0;
-  
-  // Remove
+
   const animChars = document.querySelectorAll('.-animChar');
   animChars.forEach((char, i) => {
     char.style.animationDelay = `${(animChars.length - i) * 50}ms`;
     char.classList.add('-reverse');
   });
-  
-  // Appear
-  setTimeout(() => {
-    let newInnerHTML = '';
-    for (let i = 0; i < randomTitle.length; i++) {
-      if (randomTitle[i] === ' ') {
-        newInnerHTML += `<span>${randomTitle[i]}</span>`;
-        spaceCounter++
-      } else {
-        newInnerHTML += `<span class="-animChar" style="animation-delay: ${i * 50}ms">${randomTitle[i]}</span>`;
-      }
-    }
-    swapper.innerHTML = newInnerHTML;
-  }, (animChars.length - spaceCounter) * msWait);
-  
-  setTimeout(titleChange, ((randomTitle.length - spaceCounter) * msWait) + 3000);
+
+  await new Promise(resolve => setTimeout(resolve, (animChars.length) * msWait));
+  swapper.innerHTML = randomTitle.split('').map((char, i) => char === ' ' ? `<span>${char}</span>` : `<span class="-animChar" style="animation-delay: ${i * 50}ms">${char}</span>`).join('');
+
+  setTimeout(titleChange, (randomTitle.length * msWait) + 3000);
 }
 
 titleChange();
 
-
-//Emoji
+// Emoji
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 logo.addEventListener('mousedown', async () => {
+  closeShade();
   const emoji = document.createElement('div');
   emoji.classList.add('emoji');
   emoji.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
@@ -100,8 +94,6 @@ logo.addEventListener('mousedown', async () => {
   emoji.style.top = `${top + (height / 2)}px`;
 
   document.body.appendChild(emoji);
-
-  emoji.style.transition = `all 500ms cubic-bezier(0.42, 0, 0.58, 1)`;
 
   await delay(80);
   emoji.style.transform = `scale(20)`;
